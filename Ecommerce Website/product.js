6dcf5426bad8021e47b9520d3ea82c33299e2add
JasonIlunga <151781268+JasonIlunga@users.noreply.github.com>
2024-01-11 13:18:06 -0500

    Add files via upload

---
+++ Ecommerce Website/product.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+      id: 0,
+      name: "The Cotton Wreath Sweatshirt Purple",
+      price: 118,
+      instock: 100,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "PurpleDTHoodie.webp",
+    },
+    
+    {
+        id: 1,
+        name: "	The Cotton Wreath Shorts Red",
+        price: 118,
+        instock: 43,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "RedDTShorts.webp",
+      },
+     
+      {
+        id: 1,
+        name: "The Cotton Wreath SweatshirtRed",
+        price: 118,
+        instock: 43,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "RedDTHoodiePage.html",
+      }
+    
+    ];
